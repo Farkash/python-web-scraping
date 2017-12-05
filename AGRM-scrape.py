6ff6d6a7c77b778d8e9082e053f9ba826ae419e9
STEVE FARKAS <Steve@STEVEs-MacBook-Pro.local>
2017-12-05 05:04:21 -0500

    agrm-complete

--- AGRM-scrape.py
+++ AGRM-scrape.py
@@ -42,14 +42,15 @@ page_list = [us1, us2, us3, us4, us5, us6, us7, us8]
 
 
 org = []
-street_address = []
 city = []
 state = []
-zip_code = []
 phone = []
+agrm_page_url = []
+
+street_address = []
+zip_code = []
 website = []
 email_address = []
-agrm_page_url = []
 
 
 # pull first chunks of data, including urls for each organization's page where more data can be found
@@ -81,16 +82,70 @@ for k in range(0, len(agrm_page_url)):
     org_response = s.get(agrm_page_url[k])
     # save entire HTML doc to file in case I get blocked by the server with subsequent requests
     raw_html = org_response.content
-    file = open("/Users/Steve/Desktop/agrm-pages/%s%s.html" %(k, org[k]), "w")
+    file = open("/Users/Steve/Desktop/agrm-pages/%s%s%s.html" %(k, "-", org[k]), "w")
     file.write(raw_html)
     file.close()
-    sleep(random.randint(3, 10))
+    sleep(random.randint(1,3))
 
-create list of html files to parse through
+# test_frame = pandas.DataFrame()
+# test_frame["name"] = org
+# test_frame[""]
+
+# create list of html files to parse through
+org_site_list = []
 org_site_list = os.listdir("/Users/Steve/Desktop/agrm-pages/")
 org_site_list = natsorted(org_site_list)
+del org_site_list[272]
 for p in range(0, len(org_site_list)):
     org_soup = BeautifulSoup(open("/Users/Steve/Desktop/agrm-pages/%s" %org_site_list[p]))
+    div_address = org_soup.find('div', class_='CoAddress').p
+    ugly_addr_list = div_address.text.strip().split('\n')
+    pretty_addr_list = [x.strip() for x in ugly_addr_list if x.strip() != '']
+    v_street = pretty_addr_list[0]
+    street_address.append(v_street)
+    print v_street
+    # v_city = pretty_addr_list[1].split(',')[0]
+    # city.append(v_city)
+    # print v_city
+    # v_state = pretty_addr_list[1].split(' ')[1]
+    # state.append(v_state)
+    # print v_state
+    if(len(pretty_addr_list) > 2):
+        del pretty_addr_list[1]
+    pretty_addr_inter = pretty_addr_list[1].split(',')
+    v_zip = pretty_addr_inter[1].strip().split(' ')[len(pretty_addr_inter[1].strip().split(' ')) -1]
+    zip_code.append(v_zip)
+    print v_zip
+    # website
+    div_contact = org_soup.find('div', class_='contact').p
+    ugly_contact_list = div_contact.text.strip().split('\n')
+    pretty_contact_list = [x.replace(u'\xa0', u'').strip() for x in ugly_contact_list if x.strip() != '']
+    v_website = pretty_contact_list[1][pretty_contact_list[1].find(':') +1 :].strip().encode('utf-8')   
+    website.append(v_website)
+    print v_website
+    # email_address
+    v_email = pretty_contact_list[4][pretty_contact_list[4].find(':') +1 :].strip().encode('utf-8')
+    email_address.append(v_email)
+    print v_email
+
+
+
+
+
+
+
+
+
+
+
+
+# agrm_page_url[12]
+
+# unsafe method of querying the site directly
+s = requests.Session() 
+for p in range(0, len(agrm_page_url)):
+    org_response = s.get(agrm_page_url[p])
+    org_soup = BeautifulSoup(org_response.content, "lxml")
     div_address = org_soup.find('div', class_='CoAddress').p
     ugly_addr_list = div_address.text.strip().split('\n')
     pretty_addr_list = [x.strip() for x in ugly_addr_list if x.strip() != '']
@@ -118,6 +173,10 @@ for p in range(0, len(org_site_list)):
     email_address.append(v_email)
     print v_email
 
+# agrm_page_url[83]
+agrm_page_url[8]
+
+
 # write data to frame
 master_frame = pandas.DataFrame()
 master_frame["Organization"] = org
@@ -140,7 +199,26 @@ master_frame.to_csv("/Users/Steve/Dropbox/programming/Python/web-scraping/data/a
     
     
 ########################### TESTING ############
+# bad_zip_resp = requests.get('http://www.agrm.org/assnfe/CompanyDirectory.asp?MODE=VIEW&SEARCH_TYPE=13&ID=100102')
+bad_zip_resp = requests.get('http://www.agrm.org/assnfe/CompanyDirectory.asp?MODE=VIEW&SEARCH_TYPE=13&ID=115946')
+bad_zip_soup = BeautifulSoup(bad_zip_resp.content, "lxml")
+div_address = bad_zip_soup.find('div', class_='CoAddress').p
+ugly_addr_list = div_address.text.strip().split('\n')
+pretty_addr_list = [x.strip() for x in ugly_addr_list if x.strip() != '']
+v_street = pretty_addr_list[0]
+print v_street
+# v_city = pretty_addr_list[1].split(',')[0]
+# city.append(v_city)
+# print v_city
+# v_state = pretty_addr_list[1].split(' ')[1]
+# state.append(v_state)
+# print v_state
+pretty_addr_inter = pretty_addr_list[1].split(',')
+v_zip = pretty_addr_inter[1].strip().split(' ')[len(pretty_addr_inter[1].strip().split(' ')) -1]
+print v_zip
     
+len(pretty_addr_inter[1].strip().split(' '))
+
 testset = set(org)
 
 len(testset)    
