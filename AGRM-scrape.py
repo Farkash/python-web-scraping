2be6e74b50f22cdec82599dbfbfd581da5a842be
STEVE FARKAS <Steve@STEVEs-MacBook-Pro.local>
2017-12-05 02:32:40 -0500

    agrm-prog

--- AGRM-scrape.py
+++ AGRM-scrape.py
@@ -23,17 +23,23 @@ import timeit
 from time import sleep
 import random
 import os
+from natsort import natsorted
 
 base_url = 'http://www.agrm.org//agrm/Locate_a_Mission.asp'
 # post_url = 'http://www.agrm.org/assnfe/CompanyDirectory.asp'
 # country_dict = {'clCD_CountryID':181}
 # first_page = s.post(post_url, country_dict)
 
-first_page = BeautifulSoup(open("/Users/Steve/Dropbox/programming/Python/web-scraping/AGRM/agrm-first-200.html"))
-second_page = BeautifulSoup(open("/Users/Steve/Dropbox/programming/Python/web-scraping/AGRM/agrm-rest.html"))
-page_list = [first_page, second_page]
+us1 = BeautifulSoup(open("/Users/Steve/Dropbox/programming/Python/web-scraping/AGRM/us1.html"))
+us2 = BeautifulSoup(open("/Users/Steve/Dropbox/programming/Python/web-scraping/AGRM/us2.html"))
+us3 = BeautifulSoup(open("/Users/Steve/Dropbox/programming/Python/web-scraping/AGRM/us3.html"))
+us4 = BeautifulSoup(open("/Users/Steve/Dropbox/programming/Python/web-scraping/AGRM/us4.html"))
+us5 = BeautifulSoup(open("/Users/Steve/Dropbox/programming/Python/web-scraping/AGRM/us5.html"))
+us6 = BeautifulSoup(open("/Users/Steve/Dropbox/programming/Python/web-scraping/AGRM/us6.html"))
+us7 = BeautifulSoup(open("/Users/Steve/Dropbox/programming/Python/web-scraping/AGRM/us7.html"))
+us8 = BeautifulSoup(open("/Users/Steve/Dropbox/programming/Python/web-scraping/AGRM/us8.html"))
+page_list = [us1, us2, us3, us4, us5, us6, us7, us8]
 
-scrape_start_time = timeit.default_timer()
 
 org = []
 street_address = []
@@ -44,6 +50,7 @@ phone = []
 website = []
 email_address = []
 agrm_page_url = []
+
 
 # pull first chunks of data, including urls for each organization's page where more data can be found
 for i in range(0, len(page_list)):
@@ -66,6 +73,7 @@ for i in range(0, len(page_list)):
         phone.append(v_phone)
         print v_phone
         
+
 # now pull data from each organization's page
 # make an html file for every org's page just to be safe
 s = requests.Session() 
@@ -73,16 +81,16 @@ for k in range(0, len(agrm_page_url)):
     org_response = s.get(agrm_page_url[k])
     # save entire HTML doc to file in case I get blocked by the server with subsequent requests
     raw_html = org_response.content
-    file = open("/Users/Steve/Desktop/agrm-pages/%s.html" %org[k], "w")
+    file = open("/Users/Steve/Desktop/agrm-pages/%s%s.html" %(k, org[k]), "w")
     file.write(raw_html)
     file.close()
-    sleep(random.randint(5, 20))
-        
-        
-# create list of html files to parse through
-org_site_list = os.listdir("/Users/Steve/Desktop/agrm-pages/)
+    sleep(random.randint(3, 10))
+
+create list of html files to parse through
+org_site_list = os.listdir("/Users/Steve/Desktop/agrm-pages/")
+org_site_list = natsorted(org_site_list)
 for p in range(0, len(org_site_list)):
-    org_soup = BeautifulSoup(open(org_site_list[p]))
+    org_soup = BeautifulSoup(open("/Users/Steve/Desktop/agrm-pages/%s" %org_site_list[p]))
     div_address = org_soup.find('div', class_='CoAddress').p
     ugly_addr_list = div_address.text.strip().split('\n')
     pretty_addr_list = [x.strip() for x in ugly_addr_list if x.strip() != '']
@@ -96,13 +104,13 @@ for p in range(0, len(org_site_list)):
     # state.append(v_state)
     # print v_state
     v_zip = pretty_addr_list[1].split(' ')[2]    
-    zip_code.append(z_vip)
+    zip_code.append(v_zip)
     print v_zip
     # website
     div_contact = org_soup.find('div', class_='contact').p
     ugly_contact_list = div_contact.text.strip().split('\n')
     pretty_contact_list = [x.replace(u'\xa0', u'').strip() for x in ugly_contact_list if x.strip() != '']
-    v_website = pretty_contact_list[1][pretty_contact_list[i].find(':') +1 :].strip().encode('utf-8')   
+    v_website = pretty_contact_list[1][pretty_contact_list[1].find(':') +1 :].strip().encode('utf-8')   
     website.append(v_website)
     print v_website
     # email_address
@@ -133,6 +141,10 @@ master_frame.to_csv("/Users/Steve/Dropbox/programming/Python/web-scraping/data/a
     
 ########################### TESTING ############
     
+testset = set(org)
+
+len(testset)    
+    
 test_org_page = BeautifulSoup(open("/Users/Steve/Dropbox/programming/Python/web-scraping/AGRM/test_org_page.html"))
 div_address = test_org_page.find('div', class_='CoAddress').p
 
@@ -161,9 +173,9 @@ print "Scrape time elapsed: %d" %scrape_time_elapsed
 ######################### detail test
 page = requests.get('http://www.agrm.org/assnfe/CompanyDirectory.asp?MODE=VIEW&SEARCH_TYPE=13&ID=100025')
 org_name = "The Foundry Ministries"
-
+i = 0
 raw_html = page.content
-file = open("/Users/Steve/Desktop/%s.html" %org_name, "w")
+file = open("/Users/Steve/Desktop/%s%s.html" %(i, org_name), "w")
 file.write(raw_html)
 file.close()
 org_soup = BeautifulSoup(raw_html)
@@ -193,7 +205,5 @@ print v_website
 v_email = pretty_contact_list[4][pretty_contact_list[4].find(':') +1 :].strip()
 email_address.append(v_email)
 print v_email
-
-
 
 
